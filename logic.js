b6a4048512145d2b750461983d9109cf136cfc9e
kencrimsonIT <tongkien930@gmail.com>
2025-03-07 13:14:12 +0700

    divide into 2 independent forms

--- logic.js
+++ logic.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('idForm');
+    const formContainer = document.querySelector('.form-container');
+    const resultContainer = document.querySelector('.result-container');
     const submitBtn = document.getElementById('submitBtn');
     const photoBtn = document.getElementById('photoBtn');
+    const returnBtn = document.getElementById('returnBtn');
     const resultBox = document.getElementById('result-box');
+
+    // Hide result container initially
+    resultContainer.style.display = 'none';
 
     //Information submitting button event
     submitBtn.addEventListener('click', function(event) {
@@ -13,7 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const studentID = document.getElementById('id').value.trim();
         const className = document.getElementById('class').value.trim();
         const majorName = document.getElementById('major').value.trim();
-        const studentCourse = document.getElementById('course').value.trim();
+        const courseSelector = document.getElementById('course');
+        const studentCourse = courseSelector.options[courseSelector.selectedIndex].text;
         
         // Display results
         document.getElementById('resultName').textContent = studentName;
@@ -21,6 +28,16 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('resultClass').textContent = className;
         document.getElementById('resultMajor').textContent = majorName;
         document.getElementById('resultCourse').textContent = studentCourse;
+
+        // Hide form container and show result container
+        formContainer.style.display = 'none';
+        resultContainer.style.display = 'block';
+    });
+
+    returnBtn.addEventListener('click', function () {
+        // Hide result container and show form container
+        resultContainer.style.display = 'none';
+        formContainer.style.display = 'block';
     });
 
     //Screen capturing button event
